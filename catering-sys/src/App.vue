5b6dd84b1c4b2678917986f0450d5ff30c12c285
moon <243654106@qq.com>
2019-05-16 18:28:32 +0800

    提交基础架构

---
+++ catering-sys/src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+              
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>  
+              <span>菜单一
+              </span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                 
+              <router-link class="menu-a" to="/"><span>Home</span></router-link>
+              </el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>  
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <router-link class="menu-a" to="/about">About</router-link>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header padding="0px"> 
+          <div class="user-info">
+            <i class="el-icon-user"></i>
+            <span>admin</span>
+            <el-divider direction="vertical"></el-divider>
+            <router-link class="menu-a" to="/login"><span>loginout</span></router-link>
+          </div>
+        </el-header>
+        <el-main>
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">菜单一</el-breadcrumb-item>
+            <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+          </el-breadcrumb>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style>
+html, body, #app, .el-container{
+  height: 100%;
+}
+body{
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #333333;
+}
+.menu-a{
+  color: #fff;
+  text-decoration-line: none;
+}
+.el-aside{
+  background-color: #545c64;
+}
+.el-header{
+  padding: 0;
+  float: right;
+  background-color: #ffffff;
+}
+.el-menu{
+  width: 200px;
+  text-align: left;
+  border-right: none;
+}
+
+.el-menu > a{
+  color:#fff ;
+}
+.el-container{
+  background: #f0f2f5;
+}
+#nav {
+  height: 50px;
+}
+.user-info{
+  float: right;    
+  line-height: 60px;
+  font-size: 14px;
+}
+</style>
---
+++ catering-sys/src/views/Login.vue
@@ -0,0 +1,62 @@
+<template>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="demo-ruleForm">
+        <h3>登录</h3>
+        <el-form-item label="账号" props="account">
+            <el-input v-model="ruleForm.account" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" props="password">
+            <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="loginToSys('ruleForm')">登录</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+<script>
+export default {
+    data() {
+        var validatePass = (rule, value, callback) => {
+            if(value === ''){
+                callback(new Error('请输入密码'));
+            }else{
+                callback();
+            }
+        };
+
+        var validateAccount = (rule, value, callback) => {
+            if(value === ''){
+                callback(new Error('请输入账号'));
+            }else{
+                callback();
+            }
+        };
+
+        return {
+            ruleForm: {
+                account:'',
+                password:'',
+            },
+            rules: {
+                account: [
+                    { validator: validateAccount, tigger: 'blur'}
+                ],
+                password: [
+                    { validator: validatePass, tigger: 'blur'}
+                ],
+            }
+        };
+    },
+    methods: {
+        loginToSys(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    alert("login")
+                }else{
+                    console.log("error")
+                    return false;
+                }
+            });
+        },
+    }
+}
+</script>
